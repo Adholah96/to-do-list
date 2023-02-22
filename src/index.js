c17bcca1624c52a225ab4d14fdf3b155704cd879
Adholah96 <adholah96@gmail.com>
2023-02-22 18:06:44 +0300

    Project setup

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import _ from 'lodash';
+import './style.css';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, currently included via a script, is required for this line to work
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  element.classList.add('hello');
+
+  return element;
+}
+
+document.body.appendChild(component());
