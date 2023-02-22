3b546bfee0b8ece474ac9d940a9e025b8d84306b
Adholah96 <adholah96@gmail.com>
2023-02-22 21:21:37 +0300

    Load content dynamically

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-import './style.css';
+import './style.css'
 
+const lists = document.querySelector('.to-do-item')
 
+let todoArray = [
+  {
+    description: 'wash the dishes',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'complete To Do list project',
+    completed: false,
+    index: 2,
+  },
+]
 
+const populateList = () => {
+  let displayData = todoArray.map((item) => {
+    return `
+    <div class="list-item">
+          <label for="todo"
+            ><input type="checkbox" name="todo" id="todo" /> ${item.description}</label
+          ><i class="fa-solid fa-ellipsis-vertical"></i>
+        </div>
+        <hr />
+    
+    `
+  })
+  lists.innerHTML = displayData.join(' ')
+}
 
+window.addEventListener('DOMContentLoaded', () => {
+  populateList()
+})
