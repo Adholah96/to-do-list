5a28544337f820b4032e9709fae2b213db80173b
Adholah96 <adholah96@gmail.com>
2023-02-24 10:00:35 +0300

    Make the text editable

--- modules/app.js
+++ modules/app.js
@@ -1,21 +1,21 @@
-const submit = document.getElementById('enter-list')
-const input = document.querySelector('.todo-input')
-const lists = document.querySelector('.to-do-item')
+const submit = document.getElementById('enter-list');
+const input = document.querySelector('.todo-input');
+const lists = document.querySelector('.to-do-item');
 
-let todoArray = []
+let todoArray = [];
 
 // Get data from the local storage
 const getData = () => {
-  const data = localStorage.getItem('todoArray')
+  const data = localStorage.getItem('todoArray');
   if (data) {
-    todoArray = JSON.parse(data)
+    todoArray = JSON.parse(data);
   }
-}
+};
 
 // Add data to the local storage
 const addData = () => {
-  localStorage.setItem('todoArray', JSON.stringify(todoArray))
-}
+  localStorage.setItem('todoArray', JSON.stringify(todoArray));
+};
 
 // Add item
 const addItem = (description) => {
@@ -23,10 +23,10 @@ const addItem = (description) => {
     description,
     completed: false,
     index: todoArray.length + 1,
-  }
-  todoArray.push(item)
-  addData()
-}
+  };
+  todoArray.push(item);
+  addData();
+};
 
 // display list and content on the page
 const populateList = () => {
@@ -35,8 +35,8 @@ const populateList = () => {
        <div class="list-item">
   <label for="todo-${item.index}">
     <input type="checkbox" name="todo-${item.index}" id="todo-${item.index}" ${
-      item.completed ? 'checked' : ''
-    } />
+  item.completed ? 'checked' : ''
+} />
     <p>${item.description}</p>
   </label>
 
@@ -49,85 +49,85 @@ const populateList = () => {
   </button>
 </div>
 <hr />
-    `
-  )
-  lists.innerHTML = displayData.join(' ')
+    `,
+  );
+  lists.innerHTML = displayData.join(' ');
 
   // toggle checked and unchecked on the list
-  const chbox = document.querySelectorAll('input[type="checkbox"]')
+  const chbox = document.querySelectorAll('input[type="checkbox"]');
   chbox.forEach((check, i) => {
     check.addEventListener('click', () => {
-      todoArray[i].completed = !todoArray[i].completed
-      addData(todoArray)
-    })
-  })
+      todoArray[i].completed = !todoArray[i].completed;
+      addData(todoArray);
+    });
+  });
 
   // add event listener to detail buttons
-  const detailbtn = document.querySelectorAll('.detail-btn')
+  const detailbtn = document.querySelectorAll('.detail-btn');
   detailbtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      e.preventDefault()
-      const index = parseInt(e.currentTarget.dataset.index, 10)
+      e.preventDefault();
+      const index = parseInt(e.currentTarget.dataset.index, 10);
       const deletebtn = document.querySelector(
-        `.delete-btn[data-index="${index}"]`
-      )
-      deletebtn.classList.remove('hide')
-      btn.classList.add('hide')
+        `.delete-btn[data-index="${index}"]`,
+      );
+      deletebtn.classList.remove('hide');
+      btn.classList.add('hide');
 
       // make the paragraph editable
-      const p = e.currentTarget.parentNode.querySelector('p')
-      p.contentEditable = true
-      p.focus()
-    })
-  })
+      const p = e.currentTarget.parentNode.querySelector('p');
+      p.contentEditable = true;
+      p.focus();
+    });
+  });
 
   // add event listener to delete buttons
-  const deletebtn = document.querySelectorAll('.delete-btn')
+  const deletebtn = document.querySelectorAll('.delete-btn');
   deletebtn.forEach((btn) => {
     btn.addEventListener('click', (e) => {
-      const index = parseInt(e.currentTarget.dataset.index, 10)
-      todoArray.splice(index - 1, 1)
+      const index = parseInt(e.currentTarget.dataset.index, 10);
+      todoArray.splice(index - 1, 1);
       for (let i = index - 1; i < todoArray.length; i += 1) {
-        todoArray[i].index = i + 1
+        todoArray[i].index = i + 1;
       }
-      addData()
-      populateList()
-    })
-  })
+      addData();
+      populateList();
+    });
+  });
 
   // add event listener to paragraphs for editing
-  const paragraphs = document.querySelectorAll('.list-item p')
+  const paragraphs = document.querySelectorAll('.list-item p');
   paragraphs.forEach((p, i) => {
     p.addEventListener('keydown', (e) => {
       if (e.keyCode === 13) {
-        e.preventDefault()
-        p.blur()
+        e.preventDefault();
+        p.blur();
       }
-    })
+    });
     p.addEventListener('blur', () => {
-      todoArray[i].description = p.textContent.trim
-      addData()
-    })
-  })
-}
+      todoArray[i].description = p.textContent.trim;
+      addData();
+    });
+  });
+};
 
 // add list when form is submitted
 const addlist = () => {
   submit.addEventListener('click', (e) => {
-    e.preventDefault()
-    const inputData = input.value.trim()
+    e.preventDefault();
+    const inputData = input.value.trim();
     if (inputData !== '') {
-      addItem(inputData)
-      input.value = ''
-      populateList()
+      addItem(inputData);
+      input.value = '';
+      populateList();
     }
-  })
+  });
   input.addEventListener('keydown', (e) => {
     if (e.keyCode === 13) {
-      e.preventDefault()
-      submit.click()
+      e.preventDefault();
+      submit.click();
     }
-  })
-}
+  });
+};
 
-export { getData, populateList, addlist }
+export { getData, populateList, addlist };
